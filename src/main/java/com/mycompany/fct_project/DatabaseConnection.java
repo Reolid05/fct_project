49a28cacf250ed318e408719e767522c6ac7682b
AdminDavid <AdminDavid@WKSDavid.domespiralrojo.local>
2024-06-10 18:54:14 +0200

    Login, Main, User, Database

--- src/main/java/com/mycompany/fct_project/DatabaseConnection.java
+++ src/main/java/com/mycompany/fct_project/DatabaseConnection.java
@@ -2,22 +2,23 @@ package com.mycompany.fct_project;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException; 
+import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://192.168.1.10:5432/DB_FCT";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "Virtual01";
-
+    private static final String URL = "jdbc:postgresql://192.168.1.10:5432/DB_FCT?charset=utf8";
+    private static final String USERNAME = "postgres"; 
+    private static final String PASSWORD = "Virtual01"; 
+    
     public static Connection getConnection() throws SQLException {
-        try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+
+    public static boolean testConnection() {
+        try (Connection connection = getConnection()) {
+            return true;
         } catch (SQLException e) {
-            throw new SQLException("Error al conectar a la base de datos", e);
+            e.printStackTrace();
+            return false;
         }
     }
-    
-    
-    
 }
-
